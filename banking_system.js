4d467db92f82b44769eb836cf8e4e5a266636959
KelfinTDetonator <kelfinantoni@gmail.com>
2023-09-08 03:44:20 +0700

    add: bankAccount class methods >> >>   deposit method to increase the money inside bank account >>   withdraw method to decrease the money

--- banking_system.js
+++ banking_system.js
@@ -2,6 +2,11 @@ class BankAccount{
     constructor(saldo){
         this.saldo = saldo
     }
+    deposit(amount){
+        this.saldo += amount;
+    }
 
-    
+    withdraw(amount){
+        this.saldo -= amount;
+    }
 }
