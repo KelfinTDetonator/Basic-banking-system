46c3bc42a599f2e54fc6a697303f7a1d4458a6f6
KelfinTDetonator <kelfinantoni@gmail.com>
2023-09-08 03:48:13 +0700

    refactor: deposit & withdraw method

--- banking_system.js
+++ banking_system.js
@@ -1,12 +1,20 @@
 class BankAccount{
     constructor(saldo){
-        this.saldo = saldo
+        this.saldo = +saldo;
     }
     deposit(amount){
-        this.saldo += amount;
+        setTimeout(()=>{
+            return this.saldo += amount;
+        }, 1000);
     }
 
     withdraw(amount){
-        this.saldo -= amount;
+        setTimeout(()=>{
+            return this.saldo -= amount;
+        }, 1000);
     }
 }
+
+const kelfin = new BankAccount(0);
+kelfin.deposit(50000);
+kelfin.withdraw(30000)
