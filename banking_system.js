9c84cf017b0a1828e7d4ab852ea742c514fece15
KelfinTDetonator <kelfinantoni@gmail.com>
2023-09-08 15:56:14 +0700

    feat: simple auth pin bank account

--- banking_system.js
+++ banking_system.js
@@ -1,20 +1,36 @@
 class BankAccount{
-    constructor(saldo){
-        this.saldo = +saldo;
+    constructor(pin){
+        this.saldo = 100000;
+        this.pin = this.#encrypt(pin);
     }
+    #encrypt(pin){
+        return `encrypted-pin-of${pin}`;
+    }
+
+    #decrypt(){
+        return this.pin.split(`encrypted-pin-of`)[1];
+    }
+
+    authenticate(pin){
+        this.#decrypt() === pin;
+    }
+
     deposit(amount){
         setTimeout(()=>{
-            return this.saldo += amount;
-        }, 1000);
+            console.log("Processing request..")
+            this.saldo += amount;
+        }, 500);
+        setTimeout(()=>{ console.log(`Dana sebesar ${amount} telah berhasil dideposit!`) }, 4000)
+        return this.saldo;
     }
 
     withdraw(amount){
         setTimeout(()=>{
-            return this.saldo -= amount;
-        }, 1000);
+            this.saldo -= amount; return "1";
+        }, 300);
+        return " ";
     }
 }
 
 const kelfin = new BankAccount(0);
-kelfin.deposit(50000);
-kelfin.withdraw(30000)
+kelfin.deposit(50000)
