439d52ee103a944f5a359e3187b6c525066765f4
KelfinTDetonator <kelfinantoni@gmail.com>
2023-09-08 23:29:56 +0700

    fix: display menu

--- screenClass.js
+++ screenClass.js
@@ -1,9 +1,41 @@
+import { kelfin } from "./banking_system.js";
+
 export default class Display{
     constructor(prop){
         this.prop = prop.toString();
     }
 
-    
+    tampilLayar(){
+        let quit = true;
+        do{
+            let pilih = 
+            +prompt(
+               `Masukkan pilihan dibawah ini:
+                [1]: Deposit
+                [2]: Withdraw
+                [3]: Cek saldo
+                [4]: Keluar
+                `, "");
+            
+            switch (pilih) {
+                case 1:{ 
+                    let input = +prompt("Masukkan nominal saldo", ""); 
+                    (kelfin.deposit(input))(); break;
+                }
+                case 2:{ 
+                    let input = +prompt("Masukkan nominal saldo", ""); 
+                    (kelfin.withdraw(input))(); break; 
+                }   
+                case 3:{ this.displayAmountMessage(); break; }
+                case 4:{ 
+                    quit = confirm("Apakah anda yakin ingin keluar menu?"); 
+                    break;
+                }
+                default: "Kode salah"
+                    break;
+            }
+        } while(quit !== false)
+}
 
     displayAmountMessage(){
         let digit = this.prop.length
